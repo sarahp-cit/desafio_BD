36a352cb345dce833d82e9c32072aee7b128b007
Arnald da Costa Queiroga <CIANDT\arnald@lnb036081spo.ciandt.global>
2022-07-22 12:02:37 -0300

    Backend do projeto base

---
+++ backend/src/main/java/com/totalshakes/wstotalshakes/WstotalshakesApplication.java
@@ -0,0 +1,13 @@
+package com.totalshakes.wstotalshakes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WstotalshakesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WstotalshakesApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/totalshakes/wstotalshakes/WstotalshakesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.totalshakes.wstotalshakes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WstotalshakesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
